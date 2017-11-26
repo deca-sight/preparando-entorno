3d072ca61ff00a7b5dd22064bf321abeca9bce21
Carlos Viveros <deca.sight@gmail.com>
2017-11-26 01:21:27 -0300

    Add script

---
+++ preparandoEntorno.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Instalando programas vitales
+apt-get -y install vim zsh git wget curl tmux automake
+
+#instalando dependencias
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" #instalando framework oh-my-zsh
+
+mkdir dependencias
+cd dependencias
+wget https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz #instalando libevent para tmux
+tar -zxvf libevent-2.1.8-stable.tar.gz 
+cd libevent-2.1.8-stable
+./configuration && make && make install
+
+
+wget http://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz #descargando ncurses
+tar -zxvf ncurses-6.0.tar.gz
+cd ncurses-6.0
+./configuration && make && make install
+
+git clone https://github.com/deca-sight/vim-config
+git clone https://github.com/deca-sight/tmux-config
+git clone https://github.com/deca-sight/zsh-config
