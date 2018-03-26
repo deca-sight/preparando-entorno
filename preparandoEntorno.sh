33c9426f70b4eddb50cd16c7f82876195e96e3a6
Deca Sight <deca.sight@gmail.com>
2018-03-25 23:37:16 -0300

    test script

--- preparandoEntorno.sh
+++ preparandoEntorno.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-
-#Instalando programas vitales
+# install basics programs
 apt-get -y install vim zsh git wget curl tmux automake
 
-#instalando dependencias
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" #instalando framework oh-my-zsh
+#install dependencies
 
-mkdir dependencias
-cd dependencias
-wget https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz #instalando libevent para tmux
-tar -zxvf libevent-2.1.8-stable.tar.gz 
-cd libevent-2.1.8-stable
-./configuration && make && make install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" #install framework oh-my-zsh
 
 
-wget http://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz #descargando ncurses
-tar -zxvf ncurses-6.0.tar.gz
-cd ncurses-6.0
-./configuration && make && make install
-
+#mkdir dependencias
+#cd dependencias
+#wget https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz #instalando libevent para tmux
+#tar -zxvf libevent-2.1.8-stable.tar.gz 
+#cd libevent-2.1.8-stable
+#./configuration && make && make install
+#
+#
+#wget http://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz #descargando ncurses
+#tar -zxvf ncurses-6.0.tar.gz
+#cd ncurses-6.0
+#./configuration && make && make install
+cd ~
 git clone https://github.com/deca-sight/vim-config
 git clone https://github.com/deca-sight/tmux-config
 git clone https://github.com/deca-sight/zsh-config
